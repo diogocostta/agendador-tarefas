0fb8f82b56f2438a17b328917f6b04e0ec4609d0
Diogo. <diogoc1100@gmail.com.>
2026-05-13 15:12:28 -0300

    adicionadas as configurações de segurança

---
+++ src/main/java/com/diogocosta/agendadortarefas/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.diogocosta.agendadortarefas.infrastructure.security;
+
+
+import com.diogo.usuario.infrastructure.entity.Usuario;
+import com.diogo.usuario.infrastructure.repository.UsuarioRepository;
+import com.diogocosta.agendadortarefas.business.dto.UsuarioDTO;
+import com.diogocosta.agendadortarefas.infrastructure.client.UsuarioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+
+    @Autowired
+    private UsuarioClient client;
+
+
+    public UserDetails carredaDadosUsuario (String email, String token){
+        UsuarioDTO usuarioDTO = client.buscaUsuarioPorEmail(email, token);
+        return User
+                .withUsername(usuarioDTO.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuarioDTO.getSenha()) // Define a senha do usuário
+                .build();
+    }
+}
