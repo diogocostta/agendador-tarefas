ff9b30769d5d68a79857fe3f68ee492b5196eead
Diogo. <diogoc1100@gmail.com.>
2026-05-27 15:12:06 -0300

    Adicionados métodos de DELETE, PATCH de statuse e de PUT de tarefas

--- src/main/java/com/diogocosta/agendadortarefas/controller/TarefasController.java
+++ src/main/java/com/diogocosta/agendadortarefas/controller/TarefasController.java
@@ -3,8 +3,7 @@ package com.diogocosta.agendadortarefas.controller;
 
 import com.diogocosta.agendadortarefas.business.TarefasService;
 import com.diogocosta.agendadortarefas.business.dto.TarefasDTO;
-import com.diogocosta.agendadortarefas.infrastructure.entity.TarefasEntity;
-import lombok.Data;
+import com.diogocosta.agendadortarefas.infrastructure.enums.StatusNoficacaoEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -21,22 +20,41 @@ public class TarefasController {
     private final TarefasService tarefasService;
 
     @PostMapping
-    public ResponseEntity<TarefasDTO> gravarTarefas (@RequestBody TarefasDTO dto,
-                                                     @RequestHeader("Authorization") String token){
+    public ResponseEntity<TarefasDTO> gravarTarefas(@RequestBody TarefasDTO dto,
+                                                    @RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(tarefasService.gravarTarefas(token, dto));
     }
+
     @GetMapping("/eventos")
-    public ResponseEntity<List<TarefasDTO>> buscaListaDeTarefasPorPeriodo (
+    public ResponseEntity<List<TarefasDTO>> buscaListaDeTarefasPorPeriodo(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalDateTime dataInical,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalDateTime dataFinal){
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalDateTime dataFinal) {
 
         return ResponseEntity.ok(tarefasService.buscaTarefasAgendadasPorPeriodo(dataInical, dataFinal));
     }
+
     @GetMapping
-    public ResponseEntity<List<TarefasDTO>> buscaTarefasPorEmail (@RequestHeader("Authorization") String token){
+    public ResponseEntity<List<TarefasDTO>> buscaTarefasPorEmail(@RequestHeader("Authorization") String token) {
         List<TarefasDTO> tarefas = tarefasService.buscaTarefasPorEmail(token);
         return ResponseEntity.ok(tarefas);
 
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletaTarefasPorId(@RequestParam("id") String id) {
+        tarefasService.deleTarefaPorId(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<TarefasDTO> alteraStatusNotificacao(@RequestParam("status") StatusNoficacaoEnum status,
+                                                              @RequestParam("id") String id) {
+        return ResponseEntity.ok(tarefasService.alteraStatus(status, id));
+    }
+
+    @PutMapping
+    ResponseEntity<TarefasDTO> updateTarefas(@RequestBody TarefasDTO dto, @RequestParam("id") String id) {
+        return ResponseEntity.ok(tarefasService.updateTarefas(dto, id));
     }
 
 }
